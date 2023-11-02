34e3462a05e252cc9caebf90f07030ac06a9ffd1
Anderson Gomes <anderson.informatica2016@gmail.com>
2023-11-02 18:55:11 -0300

    adicionadas funcionalidades que armazenam informações do usuário em cache e depois são recuperadas em forma de lista em HTML

---
+++ README.txt
@@ -0,0 +1,8 @@
+AndersonRFC
+Anderson Gomes
+02-11-2023
+anderson.informatica2016@gmail.com
+
+Aqui temos uma pagina web que armazena informações enviadas pelo usuário em cache.
+Isso significa que, essas informações ficarão persistidas, ou seja, disponiveis mesmo 
+que a página seja fechada ou reiniciada.
--- script.js
+++ script.js
@@ -1,21 +1,65 @@
-// LISTA DE ELEMENTOS
+// ELEMENTOS
 var input = document.getElementById("input-entry");
+var button = document.getElementById("button-entry-save");
+var ul = document.getElementById("ul-text-list");
 
-input.addEventListener("keydown", function(event) {
+// VARIÁVEIS
+
+var listaDeItens = [];
+
+input.addEventListener("keydown", function (event) {
+  var novoLi = document.createElement("li");
+
   if (event.key === "Enter") {
+    listaDeItens.push({ item: input.value });
+    novoLi.textContent = input.value;
+
+    ul.appendChild(novoLi);
+
     input.value = "";
   }
 });
 
+button.addEventListener("click", function (event) {
+  localStorage.setItem("listaDeItens", JSON.stringify(listaDeItens));
+});
+
 //-----------------------------------------------------
 
+function minhaFuncao() {
+  var listaArmazenada = localStorage.getItem("listaDeItens");
+
+  if (listaArmazenada) {
+
+    listaDeItens = JSON.parse(listaArmazenada);
+
+    console.log(listaDeItens);
+
+    listaDeItens.forEach((item) => {
+        var novoLi = document.createElement("li");
+
+        novoLi.textContent = item.item;
+
+        ul.appendChild(novoLi);
+    });
+  }
+}
+
+if (document.readyState === "complete") {
+  minhaFuncao();
+} else {
+  window.addEventListener("load", minhaFuncao);
+}
+
+//----------------------------------------------------
+
+/*
 document.addEventListener("DOMContentLoaded", function() {
 
     console.log("Página carregada e DOM pronto.");
 });
+*/
 
-
-//----------------------------------------------------
 /*
 // Defina uma lista de objetos
 var listaDeObjetos = [
@@ -44,8 +88,6 @@ if (listaArmazenada) {
 }
 */
 
-
-
 // Define um valor no Local Storage com a chave e o valor
 // localStorage.setItem("chave", "Este é um valor em cache.");
 
@@ -72,4 +114,25 @@ if (objetoDoLocalStorage) {
     var objeto = JSON.parse(objetoDoLocalStorage);
     console.log("Nome: " + objeto.nome + ", Idade: " + objeto.idade);
 }
+*/
+
+
+/*
+//Com o fetch vai dar pra usar o cache de banco de dados kkk oqqqq
+
+fetch('/seu-endpoint-de-api', {
+  method: 'POST',  // ou 'GET', dependendo do seu caso
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ key: 'value' }), // Se você está enviando dados em formato JSON
+})
+  .then(response => response.json())
+  .then(data => {
+    // Tratar a resposta do servidor (se houver)
+  })
+  .catch(error => {
+    // Lidar com erros, se houver
+  });
+
 */
